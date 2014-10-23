e5ed18fa0a68c7c28a81bf7329b349a68793e674
Dustin Sulek <dustin@Dustins-MacBook-Pro.local>
2014-10-22 21:12:41 -0400

    Logging added to nfs server install

--- install-nfs-server.rb
+++ install-nfs-server.rb
@@ -2,13 +2,14 @@ require 'optparse'
 
 class ConfigNFS
   def initialize
-    #currently does nothing, probably will be set for logging.
+    $stdout.reopen("nfsManifest.txt", "w")
+    $stderr.reopen("nfsManifest.txt", "a")
   end
 
   #Main method - runs program.
   def config
     config_firewall
-    mount_and_export
+    export
     config_nfs_startup
   end
 
@@ -16,26 +17,30 @@ class ConfigNFS
   def config_firewall
     File.open("/etc/sysconfig/network", "a") { |f| f.write("MOUNTD_PORT=4002") }
     `service nfs restart`
+     puts "mountd service configured to port 4002"
 
     ports = 4002, 2049, 111
     ports.each do |port|
       `iptables -I INPUT 1 -s 10.0.0.0/8 -p tcp -m tcp --dport #{port} -j ACCEPT`
       `iptables -I INPUT 1 -s 10.0.0.0/8 -p udp -m udp --dport #{port} -j ACCEPT`
+      puts "Port #{port} opened for 10.0.0.0/8"
     end
     `service iptables save`
     `service iptables restart`
   end
 
-  def mount_and_export
-    #File.open("/etc/fstab", "a") { |f| f.write("/home /dev/sda5 ext3 defaults 0 0") }
+  def export
     File.open("/etc/exports", "a") { |f| f.write("/home 10.0.0.0/255.0.0.0(rw)") }
 
     `exportfs -a`
+    puts "Home exports configured and exported."
   end
 
   #starts services and registers with chkconfig at run level 3
   def config_nfs_startup
     `chkconfig --level 3 nfs on`
+
+    puts "NFS configured to run on startup"
   end
 end
 
@@ -52,5 +57,3 @@ end.parse!
 nfs = ConfigNFS.new
 nfs.config
 
-p options
-p ARGV
